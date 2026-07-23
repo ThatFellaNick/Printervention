ab5345e81c0c523f00d972028a1d8ca368b217c0
ThatFellaNick <nickcarpenter14@gmail.com>
2026-07-23 12:20:27 -0500

    Prompt for admin elevation on startup

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@
 */
 
 using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Security.Principal;
 using System.Windows.Forms;
 
 namespace Printervention
@@ -15,7 +18,68 @@ namespace Printervention
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            if (!EnsureAdministrativeContext())
+            {
+                return;
+            }
+
             Application.Run(new MainForm());
+        }
+
+        private static bool EnsureAdministrativeContext()
+        {
+            if (IsAdministratorOrSystem())
+            {
+                return true;
+            }
+
+            var response = MessageBox.Show(
+                "Printervention needs administrator rights to stage print drivers, create TCP/IP ports, and install print queues. Relaunch as administrator now?",
+                "Administrator rights needed",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (response != DialogResult.Yes)
+            {
+                return true;
+            }
+
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = Application.ExecutablePath,
+                    UseShellExecute = true,
+                    Verb = "runas"
+                });
+
+                return false;
+            }
+            catch (Win32Exception)
+            {
+                MessageBox.Show("Windows did not grant administrator rights. Printervention will continue, but install actions may fail.", "Elevation cancelled", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return true;
+            }
+        }
+
+        private static bool IsAdministratorOrSystem()
+        {
+            using (var identity = WindowsIdentity.GetCurrent())
+            {
+                if (identity == null)
+                {
+                    return false;
+                }
+
+                if (identity.IsSystem)
+                {
+                    return true;
+                }
+
+                var principal = new WindowsPrincipal(identity);
+                return principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
         }
     }
 }
